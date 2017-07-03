ab6b495aaafa70f385d8f75c4b4f736253db3221
Petchesi-Iulian <iamnotacat12@gmail.com>
2017-07-03 17:15:51 +0300

    Refs #82723 WISE: Added eea.pdf && eea.cache configurations + eea.pdf body overrides for folders and normal pages.

---
+++ src/wise/content/browser/overrides.py
@@ -0,0 +1,37 @@
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from eea.pdf.themes.section.folder import Body as PDFBody
+from eea.pdf.themes.page.body import Body as PDFPageBody
+from eea.pdf.cache import cacheKey, ramcache
+from eea.pdf.themes.manual.manual import get_node_html
+
+
+class FolderBody(PDFBody):
+    """ Custom PDF body
+    """
+    template = ViewPageTemplateFile("pt/folder.body.pt")
+
+    @property
+    def pdfs(self):
+        """ Section children
+        """
+        self._depth += 1
+        if self.depth > self.maxdepth:
+            return
+
+        ajax_load = self.request.get('ajax_load', False)
+        self.request.form['ajax_load'] = True
+
+        parent_brains = self.context.aq_parent.getFolderContents()
+        print len(parent_brains)
+        for brain in parent_brains:
+            if brain.getObject() == self.context:
+                node_object = brain.getObject()
+                html = get_node_html(node_object=node_object)
+                yield html
+        self.request.form['ajax_load'] = ajax_load
+
+
+class PageBody(PDFPageBody):
+    """ Custom PDF body
+    """
+    template = ViewPageTemplateFile('pt/page.body.pt')
