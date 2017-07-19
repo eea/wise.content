6f3d450823684143f252e7bdbcf5c677b010ecda
Petchesi-Iulian <iamnotacat12@gmail.com>
2017-07-19 17:26:22 +0300

    Refs #86898 WISE: Added image content type override + changed text field to richtext.

--- src/wise/content/upgrades.py
+++ src/wise/content/upgrades.py
@@ -16,3 +16,12 @@ def upgrade_to_3(context):
 
     # need to reimport wise.content, it has updated workflow settings
     context.runImportStepFromProfile(default_profile, 'workflow')
+
+
+def upgrade_to_4(context):
+    logger.info("Upgrading to 4")
+
+    # need to reimport wise.content, it has updated type settings
+    context.runImportStepFromProfile(default_profile, 'typeinfo')
+
+    logger.info("Upgrade finished.")
