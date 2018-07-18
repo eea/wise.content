2e0102687ae036284a86f65ceef2d94179f141b7
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-07-18 17:15:59 +0300

    Refs #97050 Prepare wise for harvesting and remove shortname behavior from RichImage content type.

---
+++ src/wise/content/marshaller.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+from eea.rdfmarshaller.value import Value2Surf
+from plone.namedfile.interfaces import INamedBlobFile, INamedBlobImage
+from wise.content.contenttypes import IRichImage
+from zope.component import adapts
+
+
+class File2Surf(Value2Surf):
+    """IValue2Surf implementation for plone.namedfile.file.NamedBlobFile """
+
+    adapts(INamedBlobFile)
+
+    def __init__(self, value):
+        self.value = value.filename
+
+
+class Image2Surf(Value2Surf):
+    """IValue2Surf implementation for plone.namedfile.file.NamedBlobImage """
+
+    adapts(INamedBlobImage)
+
+    def __init__(self, value):
+        self.value = value.filename
+
+
+class RichImage2Surf(Value2Surf):
+    """IValue2Surf implementation for wise.content.contenttypes.IRichImage """
+
+    adapts(IRichImage)
+
+    def __init__(self, value):
+        import pdb; pdb.set_trace()
+        self.value = value.filename
