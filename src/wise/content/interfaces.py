b927ee9e5d6189ca189a0ad80d396f9c213a7e07
Adrian Apostol <dev.adrianapostol@gmail.com>
2018-09-18 12:06:26 +0300

    Refs #96743 - added fullwidth content type

--- src/wise/content/interfaces.py
+++ src/wise/content/interfaces.py
@@ -1,3 +1,5 @@
+from zope.interface import Interface
+from zope import schema
 # -*- coding: utf-8 -*-
 """Module where all interfaces, events and exceptions live."""
 
@@ -6,3 +8,17 @@ from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 
 class IWiseContentLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+
+class IWiseContentTypesSettings(Interface):
+    """ portal_registry IWiseContentTypes settings
+    """
+
+    fullwidthFor = schema.Tuple(
+        title=u"Fullwidth ContentTypes",
+        description=u"Enable body fullwidth class for the "
+                      "following content-types",
+        required=False,
+        value_type=schema.Choice(
+            vocabulary="plone.app.vocabularies.ReallyUserFriendlyTypes")
+    )
