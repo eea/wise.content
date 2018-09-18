ddd81f481fa793f7494467191aabd12fa43dc191
Adrian Apostol <dev.adrianapostol@gmail.com>
2018-09-18 17:33:07 +0300

    Refs #96743 - removed fullwidth contentype

--- src/wise/content/browser/misc.py
+++ src/wise/content/browser/misc.py
@@ -157,29 +157,3 @@ class IssueCreate(BrowserView):
             status_id=1,
             category_id=794,
         )
-
-
-class FullWidthContentTypes(BrowserView):
-    """ Fullwidth body class content-types
-    """
-
-    def __init__(self, context, request):
-        """ init
-        """
-        super(FullWidthContentTypes, self).__init__(context, request)
-        self.context = context
-        self.request = request
-
-    def __call__(self):
-        """ boolean if fullwidth class should be enabled for given content-type
-        """
-        fullwidth_ctypes = self.get_full_registry() or []
-        return self.context.portal_type in fullwidth_ctypes
-
-    @memoize
-    def get_full_registry(self):
-        """ content registry cache
-        """
-        registry = getUtility(IRegistry)
-        return registry.get('wise.content.interfaces.'
-                            'IWiseContentTypesSettings.fullwidthFor')
