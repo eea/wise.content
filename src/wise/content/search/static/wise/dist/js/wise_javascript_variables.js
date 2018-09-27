d75a8e90453bec242a0de11cd513a7266e0a3207
Adrian Apostol <dev.adrianapostol@gmail.com>
2018-09-27 15:08:23 +0300

    Refs #98859 - WIP - persistent form data

--- src/wise/content/search/static/wise/dist/js/wise_javascript_variables.js
+++ src/wise/content/search/static/wise/dist/js/wise_javascript_variables.js
@@ -1,2 +1,2 @@
-var external_links_open_new_window="true",mark_special_links="true",setupTabs=null,clickFirstTab=null;
+var external_links_open_new_window="true",mark_special_links="true";
 //# sourceMappingURL=wise_javascript_variables.js.map
