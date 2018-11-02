1168bfe976d79af180bf1a8cf6d48d96cee8213c
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-11-02 13:31:36 +0200

    Added translation views.

--- src/wise/content/browser/misc.py
+++ src/wise/content/browser/misc.py
@@ -1,3 +1,7 @@
+# -*- coding: utf-8 -*-
+
+import json
+import requests
 from OFS.ObjectManager import BeforeDeleteException
 from Products.CMFDefault.formlib.schema import ProxyFieldProperty
 from Products.CMFDefault.formlib.schema import SchemaAdapterBase
@@ -6,15 +10,115 @@ from Products.CMFPlone.utils import getToolByName
 from Products.Five.browser import BrowserView
 from plone.app.controlpanel.form import ControlPanelForm
 from plone.app.iterate.interfaces import ICheckinCheckoutPolicy
+from plone.api import portal
 from plone.fieldsets.fieldsets import FormFieldsets
 from redminelib import Redmine
+from requests.auth import HTTPDigestAuth
 from wise.content import _
+from zope.annotation.interfaces import IAnnotations
 from zope.component import adapts
 from zope.interface import Interface, implements
 from zope.schema import TextLine
-from plone.memoize.view import memoize
-from plone.registry.interfaces import IRegistry
-from zope.component import getUtility
+# from plone.memoize.view import memoize
+# from plone.registry.interfaces import IRegistry
+# from zope.component import getUtility
+from BTrees.OOBTree import OOBTree
+import chardet
+import transaction
+
+ANNOTATION_KEY = 'translation.msfd.storage'
+
+
+class SendTranslationRequest(BrowserView):
+    """ Sends translation request
+    """
+
+    def __call__(self):
+        if self.request.form.get('id', None):
+            return self.get_translation_from_annot()
+        text = self.request.form.get('text-to-translate', '')
+        targetLanguages = self.request.form.get('targetLanguages', ['EN'])
+        sourceLanguage = self.request.form.get('sourceLanguage', '')
+        externalReference = self.request.form.get('externalReference', '')
+        # sourceObjectLocation = self.request.form.get('sourceObject', '')
+        data = {
+            'priority': 5,
+            'callerInformation': {
+                'application': 'Marine_EEA_20180706',
+                'username': 'ipetchesi',
+            },
+            'domain': 'SPD',
+            'externalReference': externalReference,
+            'textToTranslate': text,
+            'sourceLanguage': sourceLanguage,
+            'targetLanguages': targetLanguages,
+            'destinations': {
+                'httpDestinations':
+                    ['http://office.pixelblaster.ro:3880/Plone/marine/translation-callback/'],
+                'emailDestinations':
+                    ['iulian.petchesi@eaudeweb.ro']
+                    }
+        }
+
+        dataj = json.dumps(data)
+        headers = {'Content-Type': 'application/json'}
+
+        service_url = 'https://webgate.ec.europa.eu/etranslation/si/translate'
+        result = requests.post(service_url, auth=HTTPDigestAuth(
+                 'Marine_EEA_20180706', ''),
+                 data=dataj, headers=headers)
+
+        self.request.response.headers.update(headers)
+        res = {
+            "transId": result.content,
+            "externalRefId": externalReference
+        }
+        return json.dumps(res)
+
+    def get_translation_from_annot(self):
+        key = self.request.form.get('key', '')
+        site = self.context.Plone
+        annot = IAnnotations(site, None)
+        if annot:
+            translation = annot[ANNOTATION_KEY].get(key, None)
+            if translation:
+                # try:
+                    return translation
+                # except:
+                    # import pdb; pdb.set_trace()
+
+
+class TranslationCallback(BrowserView):
+    """ Saves the translation in Annotations
+    """
+
+    def __call__(self):
+        self.saveToAnnotation()
+        return self.request.form
+
+    def saveToAnnotation(self):
+        site = portal.getSite()
+        annot = IAnnotations(site, None)
+        if ANNOTATION_KEY not in annot.keys():
+            annot[ANNOTATION_KEY] = OOBTree()
+
+        # transId = self.request.form.get('request-id', '')
+        # targetLang = self.request.form.get('target-language', '')
+        originalText = self.request.form.get('external-reference')
+
+        self.request.form.pop('request-id')
+        self.request.form.pop('target-language')
+        self.request.form.pop('external-reference')
+
+        translatedText = self.request.form.keys()[0]
+
+        encoding = chardet.detect(translatedText)['encoding']
+        # import pdb; pdb.set_trace()
+        translatedText = translatedText.decode(encoding)
+        trans_entry = {originalText: translatedText}
+        annot[ANNOTATION_KEY].update(trans_entry)
+        transaction.commit()
+
 
 class FixCheckout(BrowserView):
     """ A view to fix getBaseline error when the original item was deleted
