cfea512604b5305cd21e6de58453919f3b5d7837
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-06-27 14:53:02 +0300

    Disabled overrides

--- src/wise/content/browser/overrides.py
+++ src/wise/content/browser/overrides.py
@@ -1,41 +1,42 @@
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from plone.app.users.browser.personalpreferences import PasswordAccountPanel
-from eea.pdf.themes.section.folder import Body as PDFBody
-from eea.pdf.themes.page.body import Body as PDFPageBody
-from eea.pdf.themes.manual.manual import get_node_html
+# from plone.app.users.browser.personalpreferences import PasswordAccountPanel
+# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+# from eea.pdf.themes.section.folder import Body as PDFBody
+# from eea.pdf.themes.page.body import Body as PDFPageBody
+# from eea.pdf.themes.manual.manual import get_node_html
+#
+#
+# class FolderBody(PDFBody):
+#     """ Custom PDF body
+#     """
+#     template = ViewPageTemplateFile("pt/folder.body.pt")
+#
+#     @property
+#     def pdfs(self):
+#         """ Section children
+#         """
+#         self._depth += 1
+#         if self.depth > self.maxdepth:
+#             return
+#
+#         ajax_load = self.request.get('ajax_load', False)
+#         self.request.form['ajax_load'] = True
+#
+#         parent_brains = self.context.aq_parent.getFolderContents()
+#         print len(parent_brains)
+#         for brain in parent_brains:
+#             if brain.getObject() == self.context:
+#                 node_object = brain.getObject()
+#                 html = get_node_html(node_object=node_object)
+#                 yield html
+#         self.request.form['ajax_load'] = ajax_load
+#
+#
+# class PageBody(PDFPageBody):
+#     """ Custom PDF body
+#     """
+#     template = ViewPageTemplateFile('pt/page.body.pt')
 
 
-class FolderBody(PDFBody):
-    """ Custom PDF body
-    """
-    template = ViewPageTemplateFile("pt/folder.body.pt")
-
-    @property
-    def pdfs(self):
-        """ Section children
-        """
-        self._depth += 1
-        if self.depth > self.maxdepth:
-            return
-
-        ajax_load = self.request.get('ajax_load', False)
-        self.request.form['ajax_load'] = True
-
-        parent_brains = self.context.aq_parent.getFolderContents()
-        print len(parent_brains)
-        for brain in parent_brains:
-            if brain.getObject() == self.context:
-                node_object = brain.getObject()
-                html = get_node_html(node_object=node_object)
-                yield html
-        self.request.form['ajax_load'] = ajax_load
-
-
-class PageBody(PDFPageBody):
-    """ Custom PDF body
-    """
-    template = ViewPageTemplateFile('pt/page.body.pt')
-
-
-class PasswordAccountPanelOverride(PasswordAccountPanel):
-    template = ViewPageTemplateFile('pt/password-account-panel.pt')
+# class PasswordAccountPanelOverride(PasswordAccountPanel):
+#     template = ViewPageTemplateFile('pt/password-account-panel.pt')
